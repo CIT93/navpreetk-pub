ce64eb018cda3a991f88f1e8ae768f74c185a0c7
Navpreet Kloy <nk202796@gmail.com>
2024-08-23 10:43:20 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "navpreet";
+const myVarType = typeof(myVar);
+console.log("myVarType "+ myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will this 11 line willrun`);
+    } else {
+        console.log(`will this 13 line run`);
+    }
+}
+
+runNow ();
+runNow ();
