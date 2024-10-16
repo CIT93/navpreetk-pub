630befa83a347e4deb4d13a0e4fbddf46623c77a
Navpreet Kloy <nk202796@gmail.com>
2024-10-15 22:41:17 -0700

    set up done

---
+++ w10/main.js
@@ -0,0 +1,71 @@
+import { renderTbl } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+import { FORM, FNAME, LNAME, SUBMIT} from "./global.js";
+import { saveLS, cfpData } from "./storage.js";
+
+const start = function (first, last, houseMembers, houseSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseMembers);
+  const houseSizePTS = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push({
+    firstName: first,
+    lastName: last,
+    houseM: houseMembers,
+    houseS: houseSize,
+    houseHoldMPTS: houseHoldPTS,
+    houseSPTS: houseSizePTS,
+    cfpTotal: total,
+  });
+}
+
+renderTbl(cfpData);
+
+//  Function to validate a single field
+const validateField = function(event) {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === '') {
+    fieldError.textContent = `${fieldId} is required`;
+    event.target.classList.add('invalid');
+  } else {
+    fieldError.textContent = '';
+    event.target.classList.remove('invalid');
+  }
+};
+
+// Attach blur event listeners
+FNAME.addEventListener('blur', validateField);
+LNAME.addEventListener('blur', validateField);
+
+FORM.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (FNAME.value !== '' && LNAME.value !== '') {
+    SUBMIT.textContent = ' ';
+    start(FNAME.value, LNAME.value,  parseInt(FORM.housem.value), FORM.houses.value);
+    saveLS(cfpData);
+    renderTbl(cfpData);
+    FORM.reset();
+  } else {
+    SUBMIT.textContent = "Form requires first and last name";
+  }
+});
+
+
+const add2 = function(...a) {
+  return 2 + a[3];
+}
+
+const result = add2(1,2,3,4);
+
+// spread arguement 
+
+//IIFE
+
+const a = 3;
+
+(function(a){
+  console.log("inside IIFE");
+  console.log(a);
+})(a);
